8450a167bf18f9522e43c8750780a4faad9c40a0
Jun-Hoe Lee <jun-hoe.lee@qbic.uni-tuebingen.de>
2023-02-28 10:27:33 +0100

    Functional modified version, to account for multiple blood samples from the same patient.

--- ep_prediction_create_samplesheet.py
+++ ep_prediction_create_samplesheet.py
@@ -87,6 +87,23 @@ def modify_alleles_in_hlatyping(old_filename, new_filename):
         write_alleles_line = ";".join(alleles_list) 
         alleles_wf.write(write_alleles_line)
 
+# function to check whether tumour_analyte and blood_analyte comes from the same patient
+def check_patient_tumour_blood_analyte(vcf_tumour, analyte_dict):
+
+    tumour, blood = vcf_tumour.split("-")
+    tumour_metadata = analyte_dict.get(tumour)
+    verboseprint("tumour_metadata ",  tumour_metadata )
+    sample_tumour_ID, sample_tumour_status_label, sample_tumour_patient = tumour_metadata.split(",")
+    blood_metadata = analyte_dict.get(blood)
+    sample_blood_ID, sample_blood_status_label, sample_blood_patient = tumour_metadata.split(",")
+    verboseprint("tumour analyte check: ", tumour)
+    verboseprint("blood analyte checkL ", blood)   
+    if sample_tumour_patient == sample_blood_patient:
+        return True
+    else:
+        return False
+
+
 # function to write out tabbed lines in list for samplesheet
 def write_full_line(mod_VCF_dict):
     # from mod_VCF_dict, retrieve all necessary info to generate a tabbed line # e.g. below
@@ -97,7 +114,7 @@ def write_full_line(mod_VCF_dict):
     output_line_list = []
 
     for qbicBarcode, vcf_metadata in mod_VCF_dict.items():
-        vcf_files, allele_barcode = vcf_metadata
+        vcf_files, allele_metadata = vcf_metadata
         # generate vcf_file names, check whether exists:
         vcf_file_dir = qbicBarcode + "_" + vcf_files
         vcf_file_name = vcf_files + args.vcf_mod_suffix
@@ -107,6 +124,7 @@ def write_full_line(mod_VCF_dict):
             print("%s not found in annotated vcf files " %full_vcf_file_path)
             break
         # generate allele file name, check whether exists:
+        allele_barcode = allele_metadata.split(",")[0]
         allele_file_name = allele_barcode + "_result.txt"
         full_allele_file_path = os.path.join(args.path_abs_to_alleles, allele_barcode, allele_file_name)
         verboseprint("full_allele_file_path " , full_allele_file_path)
@@ -134,10 +152,10 @@ def write_full_line(mod_VCF_dict):
 # QHPRG588A1	7	FO13518x03_01	blood
 # QHPRG589A9	7	FO13518x02_01	primary tumor
 
-## purpose is to generate filenames manually, and check against the filenames in part C
-
 readInputFileNames = args.input_sample_metadata.readlines()[1:] # skip header
 sample_metadata_dict = OrderedDict()
+tumour_list = [] ## tumour_only_list
+
 for line in readInputFileNames:
     line = line.strip() # remove \n
     splitLine = line.split("\t")
@@ -146,58 +164,40 @@ for line in readInputFileNames:
         sample_status_label = "bl"
     else:
         sample_status_label = "tu"
+        tumour_list.append(sample_ID)
     
-    sample_patient_status = sample_status_label + "," + sample_ID + "," + sample_patient_analyte
-    if sample_patient not in sample_metadata_dict:
-        sample_metadata_dict[sample_patient] = [sample_patient_status]
-    else:
-        sample_metadata_dict[sample_patient].append(sample_patient_status)
+    # sample_patient_status = sample_status_label + "," + sample_ID + "," + sample_patient_analyte
+    # if sample_patient not in sample_metadata_dict:
+    #     sample_metadata_dict[sample_patient] = [sample_patient_status]
+    # else:
+    #     sample_metadata_dict[sample_patient].append(sample_patient_status)
+    sample_metadata = sample_ID + "," + sample_status_label + "," + sample_patient
+    sample_metadata_dict[sample_patient_analyte] = sample_metadata 
 
 verboseprint("sample_metadata_dict ", list(sample_metadata_dict.items())[:3])
+## e.g. {FO13399x01_01: "QHPRG586AJ, bl, 3" , FO13399x02_01: "QHPRG587AI, tu, 3"...}
 ## e.g. {3: ["bl, QHPRG586AJ, FO13399x01_01" , "tu, QHPRG586AJ, FO13399x02_01"]...}
-
-## create dict of tumour only samples, that will be executed by epitopeprediction pipeline
-tumour_qbic_vcf_dict = OrderedDict()
-
-for patient, patient_metadata in sample_metadata_dict.items():
-    ## sort the nested lists by status, i.e. "bl" should be first
-    sorted_sample_list = sorted(patient_metadata, key=lambda x: x[0])
-    status_blood, qbic_barcode_blood, vc_sample_blood = sorted_sample_list[0].split(",")
-    ## now get info from "tu" samples
-    for each_sample in sorted_sample_list[1:]: # skip blood sample
-        status_tumour, qbic_barcode, vc_sample = each_sample.split(",")
-        vc_sample_tumour = vc_sample + "-" + vc_sample_blood
-        # add qbic_barcode_blood, which carries the hlatyping output of the germline
-        tumour_qbic_vcf_dict[qbic_barcode] = [vc_sample_tumour, qbic_barcode_blood]
-        
-
-verboseprint("tumour_qbic_vcf_dict ", list(tumour_qbic_vcf_dict.items())[:5])
-## e.g. {QHPRG587AJ: [FO13399x02_01_FO13399x01_01, QHPRG586AI ], ...}
-
+verboseprint("tumour_list ", tumour_list)
+verboseprint("len tumour_list" , len(tumour_list))
 
 # B. walk through the directory of alleles (output from hlatyping)
 ## create a copy and modify so all alleles are listed in one line, separated by colon
 try:
     alleles_dir_list = os.listdir(args.path_abs_to_alleles)
-    # 
     verboseprint("alleles_dir_list ", alleles_dir_list)
 except:
     print("Incorrect path to directory of alleles (output of hlatyping pipeline) provided. ")
     sys.exit()
 
-
 ## copy allele files and modify 
 for allele_dir in alleles_dir_list: # allele_dir is QBIC_barcode
-    old_allele_fileName, new_allele_file_name = generate_old_new_allele_filenames(args.path_abs_to_alleles, allele_dir )
-    modify_alleles_in_hlatyping(old_allele_fileName, new_allele_file_name ) 
+    if allele_dir.startswith("."): ## skip hidden files
+        continue
+    else:
+        old_allele_fileName, new_allele_file_name = generate_old_new_allele_filenames(args.path_abs_to_alleles, allele_dir )
+        modify_alleles_in_hlatyping(old_allele_fileName, new_allele_file_name ) 
 
-
-# C. compile all information into a list for writing samplesheet
-
-## example of output_sample_sheet
-# sample    alleles mhc_class   filename
-# 3 QHPRG586AJ_FO13399x01_01    I   [annotated vcf file from sarek location]
-# 3 QHPRG5867J_FO13399x02_01    I   ..
+# C. walk through vcf_dir, match the alleles from blood samples (hlatyping) to the VCFs
 
 try:
     vcf_dir_list = os.listdir(args.path_abs_to_annotated_vcf)
@@ -206,19 +206,54 @@ except:
     print("Incorrect path to directory of annotated vcfs (output of sarek pipeline) provided. ")
     sys.exit()
 
+## create dict of tumour only samples
+
+tumour_qbic_vcf_dict = OrderedDict()
+
+for qbic_analyte_folders in vcf_dir_list: ##path to annotated VEPs
+    ## the VCF directories are named <qbic-barcode>_<tumour_analyte>-<blood_analyte>
+    if qbic_analyte_folders.startswith("."): ## skip hidden files
+        continue
+    qbic_barcode, vc_sample_tumour = qbic_analyte_folders.split("_",1) # split by first occurence
+    if not qbic_barcode in tumour_list: ## we only want tumour samples
+        verboseprint("%s not found in tumour_list " %qbic_barcode)
+        continue
+    ## check that both tumour_analyte and blood_analyte belong to the same patient
+    if not check_patient_tumour_blood_analyte(vc_sample_tumour, sample_metadata_dict):
+        break
+    ## get corresponding qbic_barcode of the blood sample for hlatyping output
+    blood_analyte = qbic_analyte_folders.split("-")[-1]
+    qbic_barcode_blood = sample_metadata_dict.get(blood_analyte)
+    ## combine all information and store in tumour_qbic_vcf_dict
+    tumour_qbic_vcf_dict[qbic_barcode] = [vc_sample_tumour, qbic_barcode_blood]
+
+verboseprint("tumour_qbic_vcf_dict ", list(tumour_qbic_vcf_dict.items())[:5])
+## e.g. {QHPRG587AJ: [FO13399x02_01_FO13399x01_01, QHPRG586AI ], ...}
+
+
+# D. write out samplesheet
+## pair the correct hlatyping(blood) with the correct annotated VEP (tumours) 
+
+## example of output_sample_sheet
+# sample    alleles mhc_class   filename
+# 3 QHPRG586AJ_FO13399x01_01    I   [annotated vcf file from sarek location]
+# 3 QHPRG5867J_FO13399x02_01    I   ..
+
 write_lines_list = write_full_line(tumour_qbic_vcf_dict)
 
-
-# D. import write_lines_list into pandas,export as csv
+## import write_lines_list into pandas,export as csv
 header_names = ["sample","alleles","mhc_class","filename"]
 
 # samplesheet_df = pd.read_csv(StringIO(write_lines_list), sep='\t', header=header_names)
 samplesheet_df = pd.read_csv(io.StringIO('\n'.join(write_lines_list)), names = header_names, sep="\t")
 
-verboseprint("samplesheet_df head ", samplesheet_df.head())
+# sort by column "sample"
+samplesheet_sort_df = samplesheet_df.sort_values(["sample"])
+
+verboseprint("samplesheet_sort_df head ", samplesheet_sort_df.head())
 
 ## write to csv
-samplesheet_df.to_csv(args.output_samplesheet, index=False)
+samplesheet_sort_df.to_csv(args.output_samplesheet, index=False)
 
 
 
