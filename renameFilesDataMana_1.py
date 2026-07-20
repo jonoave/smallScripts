796c14d95eb8595a08cb4bd64aafaa5b89bc1921
Jun-Hoe Lee <jun-hoe.lee@qbic.uni-tuebingen.de>
2026-07-20 10:53:14 +0200

    Working version.

--- renameFilesDataMana_1.py
+++ renameFilesDataMana_1.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
+# -*- coding: latin-1 -*-
 # Jun-Hoe, Lee (2021)
 # script to rename sample files based on metadata file (tsv) from DataManager (2026)
 # input: i.tab-delimited file (txt) converted from an Excel Data Manager downloaed
-# ii. location to start renaming
+# ii. location of files to start renaming
 # usage:
 # python renameFilesDataMana_1.py [list_filenNames] [full_path to directory] -verbose
 # WARNING! Make backup before executing script to rename!
@@ -26,10 +27,13 @@ class errorDisplayParser(argparse.ArgumentParser):
 
 parser = errorDisplayParser(description='Renames all files recursively in a directory based on a list of old/new names \
                                          WARNING! Make backup before executing script!')
-parser.add_argument('inputFileNames', action="store", type=argparse.FileType('r'),
-                     help='tsv file: QBIC_barcodes\'\t\'Old_names')
+parser.add_argument('inputFileNames', action="store",
+                     help='Measurement file (txt) from Data Manage')
 parser.add_argument('dirPath', action='store',
-                     help='path to directory')
+                     help='path to directory of files to rename')
+parser.add_argument('-sampleNamesColumn', action='store',
+                     help='column name in inputFileNames of the sample names to be replaced',
+                     default="Measurement Name")
 parser.add_argument('-verbose', action='store_true', default=False,
                     help='turns on verbose mode. Usage: -verbose')  # verbose flag
 
@@ -44,17 +48,18 @@ else:
     verboseprint = lambda *a: None  # do-nothing function
 
 #########################################################
-# A. read in input file Renames
+# A. read in input metadata file
 
 # create dictionary of QBiC_barcodes: given_names
-## the QBiC_barcodes are also the folder names 
-readInputFileNames = args.inputFileNames.readlines()
-qbicRename_dict = {}
-for line in readInputFileNames:
-    line = line.strip() # remove \n
-    splitLine = line.split("\t")
-    qbicRename_dict[splitLine[1]] = splitLine[0]
+inputFile_df = pd.read_csv(args.inputFileNames, sep='\t', header=0, encoding="utf-16")
+verboseprint("inputFile_df ", inputFile_df.head())
+             
+# Measurement ID* QBiC Sample Id* Sample Name  Sample Pool Group Measurement Name  ...            Flow Cell Sequencing Run Protocol     Index i7     Index i5 Comment
+#0  NGSQ2W360001AN-22108713523922     Q2W360001AN         HR2                NaN     26037a001_01  ...  Illumina NovaSeq SP             28+10+10+90  SI-TT-C3-i7  SI-TT-C3-i5     NaN
+#1  NGSQ2W360002AW-22109409233281     Q2W360002AW 
 
+## create dictionary of SampleName: QBiC Sample Id*
+qbicRename_dict = dict(zip(inputFile_df[args.sampleNamesColumn], inputFile_df['QBiC Sample Id*']))
 verboseprint("qbicRename_dict ", list(qbicRename_dict.items())[:5])
 
 # B. get file list 
